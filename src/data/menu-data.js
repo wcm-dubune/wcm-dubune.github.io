f18aedc75487f5359592568eae902f6b509b340d
wcm-dubune <wcm.dubune@gmail.com>
2019-01-28 21:59:31 +0900

    Update: update menu info

--- src/data/menu-data.js
+++ src/data/menu-data.js
@@ -1,38 +1,38 @@
 export default {
 	left: {
-		notice: '모든 식사류에는 <span class="bold">콩나물밥</span>이 기본 제공됩니다.',
+		notice: '모든 식사류에는 <span class="bold">콩나물밥</span> 기본제공',
 		groups: [
 			{
 				subTitle: '식사류',
 				menus: [
 					{
-						name: '두부 전골',
+						name: '두부전골',
 						price: '14,000원',
-						thumb: '1.jpg',
+						// thumb: '1.jpg',
 						desc: '1인분 이상 주문 가능'
 					},
 					{
-						name: '얼큰 순두부',
+						name: '얼큰순두부',
 						price: '9,000원',
-						thumb: '1.jpg',
+						// thumb: '1.jpg',
 						desc: ''
 					},
 					{
 						name: '즉석순두부',
 						price: '8,000원',
-						thumb: '1.jpg',
+						// thumb: '1.jpg',
 						desc: ''
 					},
 					{
 						name: '청국장',
 						price: '9,000원',
-						thumb: '1.jpg',
+						// thumb: '1.jpg',
 						desc: ''
 					},
 					{
-						name: '수제 돈까스',
+						name: '수제돈까스',
 						price: '9,000원',
-						thumb: '1.jpg',
+						// thumb: '1.jpg',
 						desc: ''
 					}
 				]
@@ -41,15 +41,9 @@ export default {
 				subTitle: '추가메뉴',
 				menus: [
 					{
-						name: '고기 추가',
-						price: '10,000원',
-						thumb: '1.jpg',
-						desc: ''
-					},
-					{
 						name: '돌솥밥 추가',
 						price: '2,000원',
-						thumb: '1.jpg',
+						// thumb: '1.jpg',
 						desc: ''
 					}
 				]
@@ -62,33 +56,33 @@ export default {
 				subTitle: '메인 메뉴',
 				menus: [
 					{
-						name: '홍어 삼합',
-						price: '30,000원',
-						thumb: '1.jpg',
+						name: '홍어삼합',
+						price: '40,000원',
+						// thumb: '1.jpg',
 						desc: '두부 + 고기 + 홍어'
 					},
 					{
 						name: '홍어',
 						price: '15,000원',
-						thumb: '1.jpg',
+						// thumb: '1.jpg',
 						desc: ''
 					},
 					{
 						name: '수육',
 						price: '10,000원',
-						thumb: '1.jpg',
+						// thumb: '1.jpg',
 						desc: ''
 					},
 					{
 						name: '모두부',
 						price: '10,000원',
-						thumb: '1.jpg',
+						// thumb: '1.jpg',
 						desc: ''
 					},
 					{
 						name: '메밀전병',
 						price: '10,000원',
-						thumb: '1.jpg',
+						// thumb: '1.jpg',
 						desc: ''
 					}
 				]
@@ -103,53 +97,48 @@ export default {
 					{
 						name: '소주',
 						price: '4,000원',
-						thumb: '1.jpg',
+						// thumb: '1.jpg',
 						desc: ''
 					},
 					{
 						name: '맥주',
 						price: '4,000원',
-						thumb: '1.jpg',
+						// thumb: '1.jpg',
 						desc: ''
 					},
 					{
 						name: '막걸리',
 						price: '3,000원',
-						thumb: '1.jpg',
+						// thumb: '1.jpg',
 						desc: ''
 					},
 					{
 						name: '음료수',
 						price: '2,000원',
-						thumb: '1.jpg',
+						// thumb: '1.jpg',
 						desc: ''
 					}
 				]
 			},
 			{
-				subTitle: '계절메뉴(하계)',
+				subTitle: '계절메뉴',
 				menus: [
 					{
-						name: '콩국수',
+						name: '콩국수(하계)',
 						price: '9,000원',
-						thumb: '1.jpg',
+						// thumb: '1.jpg',
 						desc: ''
 					},
 					{
-						name: '도토리묵 무침',
-						thumb: '1.jpg',
+						name: '도토리묵 무침(하계)',
+						// thumb: '1.jpg',
 						price: '10,000원',
 						desc: ''
-					}
-				]
-			},
-			{
-				subTitle: '계절메뉴(동계)',
-				menus: [
+					},
 					{
-						name: '생굴',
+						name: '생굴(동계)',
 						price: '10,000원',
-						thumb: '1.jpg',
+						// thumb: '1.jpg',
 						desc: ''
 					}
 				]
