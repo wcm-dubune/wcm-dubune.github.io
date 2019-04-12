d831cbf02f5e9d12fe0507529a9d43e9469f5b5a
wcm-dubune <wcm.dubune@gmail.com>
2019-04-12 23:47:10 +0900

    update images & menus

--- src/data/menu-data.js
+++ src/data/menu-data.js
@@ -8,7 +8,7 @@ export default {
 					{
 						name: '두부전골',
 						price: '14,000원',
-						thumb: '수정_전골2.jpg',
+						thumb: '전골.jpg',
 						desc: '2인분 이상 주문 가능',
 						prefix: {
 							text: 'Best',
@@ -18,19 +18,19 @@ export default {
 					{
 						name: '얼큰순두부',
 						price: '9,000원',
-						thumb: '수정_얼큰.jpg',
+						// thumb: '수정_얼큰.jpg',
 						desc: ''
 					},
 					{
 						name: '즉석순두부',
 						price: '8,000원',
-						thumb: '수정_순두부.jpg',
+						thumb: '순두부.jpg',
 						desc: ''
 					},
 					{
 						name: '청국장',
 						price: '9,000원',
-						thumb: '수정_청국장.jpg',
+						// thumb: '수정_청국장.jpg',
 						desc: ''
 					},
 					{
@@ -45,9 +45,9 @@ export default {
 				subTitle: '추가메뉴',
 				menus: [
 					{
-						name: '돌솥밥 추가',
-						price: '2,000원',
-						// thumb: '1.jpg',
+						name: '콩나물밥 추가',
+						price: '3,000원',
+						thumb: '콩나물밥.jpg',
 						desc: ''
 					}
 				]
@@ -62,7 +62,7 @@ export default {
 					{
 						name: '홍어삼합',
 						price: '40,000원',
-						thumb: '수정_홍어삼합.jpg',
+						thumb: '홍어삼합.jpg',
 						desc: '홍어 + 고기 + 김치',
 						prefix: {
 							text: '추천',
@@ -92,7 +92,17 @@ export default {
 						price: '10,000원',
 						// thumb: '1.jpg',
 						desc: ''
-					}
+					},
+					{
+						name: '도토리묵 무침',
+						// thumb: '1.jpg',
+						price: '10,000원',
+						desc: '',
+						// prefix: {
+						// 	text: '여름',
+						// 	class: 'summer'
+						// }
+					},
 				]
 			}
 		]
@@ -141,16 +151,16 @@ export default {
 							class: 'summer'
 						}
 					},
-					{
-						name: '도토리묵 무침',
-						// thumb: '1.jpg',
-						price: '10,000원',
-						desc: '',
-						prefix: {
-							text: '여름',
-							class: 'summer'
-						}
-					},
+					// {
+					// 	name: '도토리묵 무침',
+					// 	// thumb: '1.jpg',
+					// 	price: '10,000원',
+					// 	desc: '',
+					// 	prefix: {
+					// 		text: '여름',
+					// 		class: 'summer'
+					// 	}
+					// },
 					{
 						name: '생굴',
 						price: '10,000원',
